9fc718d24578a652cf2fb3b1e190fdf5520d540a
g128kato <g520128u@mails.cc.ehime-u.ac.jp>
2022-03-13 18:18:11 +0900

    ログイン機能作成

---
+++ src/components/GoogleLogin.vue
@@ -0,0 +1,23 @@
+<script>
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth"
+
+const provider = new GoogleAuthProvider()
+provider.addScope("https://www.googleapis.com/auth/contacts.readonly")
+
+const auth = getAuth()
+auth.languageCode = "it"
+signInWithRedirect(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result)
+    credential.accessToken
+    result.user
+    // ...
+  })
+  .catch((error) => {
+    error.code
+    error.message
+    error.email
+    GoogleAuthProvider.credentialFromError(error)
+    // ...
+  })
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <GoogleLogin msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GoogleLogin from "@/components/GoogleLogin.vue"
+
+export default {
+  name: "Login",
+  components: {
+    GoogleLogin,
+  },
+}
+</script>
