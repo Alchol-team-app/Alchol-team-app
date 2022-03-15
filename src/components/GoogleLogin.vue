a1fb9c2617e350ceafa7293fe0e4ce20ff8e9466
g128kato <g520128u@mails.cc.ehime-u.ac.jp>
2022-03-15 14:51:55 +0900

    ログインボタン作成

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -1,23 +1,32 @@
+<template>
+  <button @click="login">Googleログイン</button>
+</template>
+
 <script>
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 
-const provider = new GoogleAuthProvider()
-provider.addScope("https://www.googleapis.com/auth/contacts.readonly")
-
-const auth = getAuth()
-auth.languageCode = "it"
-signInWithRedirect(auth, provider)
-  .then((result) => {
-    const credential = GoogleAuthProvider.credentialFromResult(result)
-    credential.accessToken
-    result.user
-    // ...
-  })
-  .catch((error) => {
-    error.code
-    error.message
-    error.email
-    GoogleAuthProvider.credentialFromError(error)
-    // ...
-  })
+export default {
+  methods: {
+    login() {
+      const provider = new GoogleAuthProvider()
+      provider.addScope("https://www.googleapis.com/auth/contacts.readonly")
+      const auth = getAuth()
+      auth.languageCode = "it"
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result)
+          credential.accessToken
+          result.user
+          // ...
+        })
+        .catch((error) => {
+          error.code
+          error.message
+          error.email
+          GoogleAuthProvider.credentialFromError(error)
+          // ...
+        })
+    },
+  },
+}
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <GoogleLogin msg="Welcome to Your Vue.js App" />
+  <div class="login">
+    <GoogleLogin />
   </div>
 </template>
 
